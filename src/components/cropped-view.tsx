1dfd867b94223f873924a906f75e282f8979a7be
Yukiniro <yukiniro@hotmail.com>
2024-02-28 22:17:17 +0800

    feat: support jpg and png download

--- src/components/cropped-view.tsx
+++ src/components/cropped-view.tsx
@@ -2,7 +2,15 @@ import useDomRect from "@/hooks/useDomRect";
 import { imageLoadableAtom, renderRelativeRectAtom } from "@/store/atoms";
 import { fitSize } from "bittydash";
 import { useAtomValue } from "jotai";
-import React, { useEffect } from "react";
+import { download } from "downloadmejs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import React, { useCallback, useEffect } from "react";
 
 export function CroppedView() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
@@ -31,11 +39,36 @@ export function CroppedView() {
     }
   }, [validImage, relativeRect, containerRect]);
 
+  const downloadJPG = useCallback(() => {
+    if (canvasRef.current) {
+      download(canvasRef.current.toDataURL("image/jpeg"), { name: "cropped.jpg" });
+    }
+  }, []);
+
+  const downloadPNG = useCallback(() => {
+    if (canvasRef.current) {
+      download(canvasRef.current.toDataURL("image/png"), { name: "cropped.png" });
+    }
+  }, []);
+
   if (validImage && relativeRect) {
     return (
-      <div className="w-1/2 h-full flex items-center justify-center" ref={containerRef}>
-        <canvas ref={canvasRef} />
-      </div>
+      <>
+        <div className="w-1/2 h-full relative flex items-center justify-center" ref={containerRef}>
+          <canvas ref={canvasRef} />
+          <div className="absolute translate-x-full -right-4 top-0">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="outline">Download</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start">
+                <DropdownMenuItem onClick={downloadJPG}>JPG</DropdownMenuItem>
+                <DropdownMenuItem onClick={downloadPNG}>PNG</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </>
     );
   }
 
